2eadfa024d98c4668ff38a26d5eae98e66553e5e
Brendan Porter <bporter@mytech.com>
2018-08-17 15:59:19 -0500

    working on saving and recalling canvas

--- storage.go
+++ storage.go
@@ -14,7 +14,7 @@ func saveCanvasAsImage() {
 	images = append(images, pixels)
 	log.Printf("Image 0, pixel 0,0: %#v", images[0][0][0])
 	for i, image := range images {
-		log.Print("Image %d pixel 0,0 is: %v", i, image[0][0])
+		log.Printf("Image %d pixel 0,0 is: %v", i, image[0][0])
 	}
 }
 
