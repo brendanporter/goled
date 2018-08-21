fdaaee584bae896d53cec5187c1eafaa1d6c1ea4
Brendan Porter <bporter@mytech.com>
2018-08-21 12:35:07 -0500

    continued work on getImages()

--- led.go
+++ led.go
@@ -362,6 +362,7 @@ func baseHandler(w http.ResponseWriter, req *http.Request) {
 	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/spectrum/1.8.0/spectrum.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
 	<script type='text/javascript'>
 
