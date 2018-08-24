1c015898888fad3b1679ed914f7bd810c6e5878d
Brendan Porter <bporter@mytech.com>
2018-08-24 08:58:26 -0500

    fixing css sizes to be more sane

--- led.go
+++ led.go
@@ -836,7 +836,9 @@ func baseHandler(w http.ResponseWriter, req *http.Request) {
 	</style>
 	</head>
 	<body>
-	<div class='container'>
+	<div class='container-fluid'>
+		<div class='row'>
+		<div class='col-sm-12'>
 		<input id='color' type='color' />
 		<button class='pallette btn btn-sm btn-danger' onclick="$('#color').spectrum('set', 'rgb(255,0,0)');setColor();">Red</button>
 		<button class='pallette btn btn-sm btn-success' onclick="$('#color').spectrum('set', 'rgb(0,255,0)');setColor();">Green</button>
@@ -860,6 +862,8 @@ func baseHandler(w http.ResponseWriter, req *http.Request) {
 				</span>
 				<div id='animations'></div>
 			</div>
+		</div>
+		</div>
 		</div>
 
 	</div>
